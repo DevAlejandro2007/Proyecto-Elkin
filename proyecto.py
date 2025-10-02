d4fbab5e0180175180a9f6d6c23218657fcfd9b1
DevAlejandro <alejandrorojasbenitez2018@gmail.com>
2025-10-02 17:33:18 -0500

    inicio

---
+++ proyecto.py
@@ -0,0 +1,9 @@
+"""
+EL PROYECTO ES:
+
+Simular una entidad financiera que gestiona creditos estudiantiles {puede ser propia de la U 
+o tambien puede ser externa, la idea es implementar todos los tipos de bases de datos que hemos 
+utilizado hasta la fecha}
+
+"""
+
