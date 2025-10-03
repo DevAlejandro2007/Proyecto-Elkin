b5de57beb382c3ca96be49c21d4c89b3107a0b9a
Andres <andresezcobar.p13@gmail.com>
2025-10-03 09:22:11 -0500

    actualizacion_andres

--- vairables.py
+++ vairables.py
@@ -9,6 +9,7 @@ utilizado hasta la fecha}
 print("Alejandro Rojas Benitez")
 print("Johan Yesid Tavera Zapata")
 print("Jose Manuel Mesa Ospina")
+print("Andrés Felipe Escobar Patiño")
 
 import numpy as np
 np.random.seed(42)
@@ -20,11 +21,19 @@ Carrera = np.random.choice([1, 2, 3, 4, 5], n_samples, p=[0.6, 0.2, 0.10, 0.05, 
 # 1=Veterinaria, 2=Ingeniería, 3=Administración, 4=Derecho, 5=Psicología
 Semestre_actual = np.random.randint(2, 10, n_samples)
 Promedio = np.round(np.random.uniform(3.5, 5.0, n_samples), 2)
+Situacion_laboral_estudiante = np.random.choice([0, 1, 2], n_samples, p=[0.7, 0.25, 0.05])
+Ingreso_propio_estudiante = np.random.randint(0, 2500000, n_samples)
+Historial_crediticio = np.random.choice([1, 2, 3], n_samples, p=[0.7, 0.2, 0.1])
+Estado_civil = np.random.choice([1, 2, 3, 4], n_samples, p=[0.6, 0.2, 0.15, 0.05])
 Ingresos_mensuales_familia = np.random.randint(1600000, 5000000, n_samples)
 Valor_credito_solicitado = np.random.randint(3000000, 30000000, n_samples)
 Tasa_interés = np.round(np.random.uniform(0.8, 2.0, n_samples), 2)
 Garantía = np.random.choice([1, 2], n_samples, p=[0.5, 0.5])
 Plazo = np.random.randint(24, 60, n_samples)
+Tipo_vivienda = np.random.choice(
+    [1, 2, 3, 4], n_samples, p=[0.3, 0.4, 0.25, 0.05]
+)
+Estado_credito = np.random.choice([1, 2, 3, 4], n_samples, p=[0.7, 0.15, 0.1, 0.05])
 Cuota_mensual = (Valor_credito_solicitado * (1 + (Tasa_interés/100)*Plazo)) // Plazo
 Monto_pagado = np.random.randint(0, Valor_credito_solicitado//2, n_samples)
 Saldo_pendiente = Valor_credito_solicitado - Monto_pagado
