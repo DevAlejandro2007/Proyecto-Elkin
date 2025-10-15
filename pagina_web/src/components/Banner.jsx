b33adc1c05873dd4b592b7a40c48e0c00ca2b78d
johanyesid129@gmail.com <keihakkon129@gmail.com>
2025-10-14 22:48:07 -0500

    Yesid

--- pagina_web/src/components/Banner.jsx
+++ pagina_web/src/components/Banner.jsx
@@ -8,7 +8,12 @@ export function Banner({title="Coopla"}) {
         <h1 className="font-bold text-4xl italic hover:text-blue-700 transition-colors duration-300 ease-in-out cursor-default">{title}</h1>
       </div>
 
-        <button className="flex p-4 text-center justify-center items-center bg-fuchsia-950 hover:bg-fuchsia-50 text-white hover:text-blue-800 rounded-xl font-bold w-1/10 h-10/12 transition-colors duration-700 ease-in-out" >Creditos</button>
+      <div className="flex justify-end items-center gap-4 w-1/3 h-full">
+        <button className="flex p-4 text-center justify-center items-center bg-fuchsia-950 hover:bg-fuchsia-50 text-white hover:text-blue-800 rounded-xl font-bold w-1/10 min-w-[200px] min-h-[50px] h-10/12 transition-colors duration-700 ease-in-out" >Creditos</button>
+        <button className="flex p-4 text-center justify-center items-center bg-fuchsia-950 hover:bg-fuchsia-50 text-white hover:text-blue-800 rounded-xl font-bold w-1/10 min-w-[200px] min-h-[50px] h-10/12 transition-colors duration-700 ease-in-out" >Inicia Sesión</button>
+      </div>
+
+      
     </div>
   )
 }
--- pagina_web/src/components/Info.jsx
+++ pagina_web/src/components/Info.jsx
@@ -1,9 +1,9 @@
 import { Button } from "./ui/Button";
 
-export function Info() {
+export function Info({className}) {
   return (
-    <div className="flex flex-1 items-center justify-center flex-col text-center p-8 gap-4 [&>p]:text-md [&>p]:text-gray-600 w-1/2 self-center mt-8 space-y-4">
-      <h2 className="font-bold text-xl ">¡Saca tu credito con nosotros!</h2>
+    <div className={`flex flex-1 items-center justify-center flex-col text-center p-8 gap-4 [&>p]:text-md [&>p]:text-gray-600 w-1/2 self-center mt-8 space-y-4 [&>p]:text-2xl ${className}`}>
+      <h2 className="font-bold text-3xl">¡Saca tu credito con nosotros!</h2>
       <p>En Coopla puedes sacar un prestamo estudiantil en base a tus posibilidades y necesidades en minutos. Dale click al boton de solicitar un credito para más información.</p>
       <Button>Solicitar Credito</Button>
     </div>
